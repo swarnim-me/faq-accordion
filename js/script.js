eb24696f8f3e6072de9792740e849259409a6cf0
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-03-30 20:32:10 +0530

    Code clean up and fix animation issues

--- js/script.js
+++ js/script.js
@@ -2,16 +2,12 @@ window.addEventListener("DOMContentLoaded", () => {
     const plusBtn = Array.from(document.querySelectorAll(".question img"));
     plusBtn.forEach((btn) => {
         const answer = btn.parentElement.parentElement.children[1];
-        const containerHeight = answer.clientHeight;
-        answer.style.height = "0px";
         btn.addEventListener("click", () => {
-            console.log(btn.parentElement.children[1]);
-            if (answer.style.height == "0px") {
-                answer.style.height = containerHeight + "px";
+            answer.classList.toggle("expanded");
+            if (answer.classList.contains("expanded")) {
                 btn.setAttribute("src", "../assets/images/icon-minus.svg");
             }
             else {
-                answer.style.height = "0px";
                 btn.setAttribute("src", "../assets/images/icon-plus.svg");
             }
         })
